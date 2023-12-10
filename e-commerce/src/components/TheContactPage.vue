7f7e00660c5121a2bc9495cb25b99c7c21245594
Zulaizat75 <m.zulaizat96@gmail.com>
2023-12-10 16:20:01 +0800

    asgn

--- e-commerce/src/components/TheContactPage.vue
+++ e-commerce/src/components/TheContactPage.vue
@@ -3,33 +3,46 @@
   <p>Use the form below to drop us an e-mail</p>
   <div id="app">
     <form>
-      <label for="firstName">Name:</label>
-      <input type="text" id="firstName" v-model="formData.firstName" required placeholder="First Name*"/>
-
-      <label for="lastName">Name:</label>
-      <input type="text" id="lastName" v-model="formData.lastName" required placeholder="Last Name*"/>
-
-      <label for="email">Email:</label>
-      <input type="email" id="email" v-model="formData.email" placeholder="Email*" />
-
-      <label for="mobile">Phone Number:</label>
-      <input type="tel" id="mobile" v-model="formData.mobile" placeholder="Phone Number" />
-
-      <label for="website">Website:</label>
-      <input type="text" id="website" v-model="formData.website" required placeholder="Website"/>
-
-      <label for="company">Company:</label>
-      <input type="text" id="company" v-model="formData.company" required placeholder="Company"/>
-
-      <label for="message">What Can We Do For You?</label>
-      <textarea id="message" v-model="formData.message" placeholder="What Can We Do For You?"></textarea>
-
-      <button type="submit">Submit</button>
+      <tr>
+        <td><label for="firstName"></label>
+          <input type="text" id="firstName" v-model="formData.firstName" placeholder="First Name*" required/>
+        </td>
+        <td>
+          <label for="lastName"></label>
+          <input type="text" id="lastName" v-model="formData.lastName" placeholder="Last Name*" required />
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <label for="email"></label>
+          <input type="email" id="email" v-model="formData.email" placeholder="Email*" required/>
+        </td>
+        <td>
+          <label for="mobile"></label>
+          <input type="tel" id="mobile" v-model="formData.mobile" placeholder="Phone Number" />
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <label for="website"></label>
+          <input type="text" id="website" v-model="formData.website" placeholder="Website" />
+        </td>
+        <td>
+          <label for="company"></label>
+          <input type="text" id="company" v-model="formData.company" placeholder="Company" />
+        </td>
+      </tr>
+      <tr>
+        <label for="message">What Can We Do For You?</label>
+        <textarea id="message" v-model="formData.message" placeholder="What Can We Do For You?"></textarea><br>
+        <button type="submit">Send It</button>
+      </tr>
     </form>
 
     <div v-if="submitted">
       <p>Thank you for reaching out! We'll get back to you soon.</p>
     </div>
+
   </div>
 </template>
 
@@ -67,5 +80,29 @@ export default {
 </script>
 
 <style scoped>
+form{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 50%;
+    margin: auto;
+    background-color: aliceblue;
+    padding: 2rem;
+    border-radius: 8px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+input, textarea {
+    width: 100%;
+    padding: 1rem;
+    margin-top: 5px;
+    margin-bottom: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+  }
 
+  button{
+    padding: 1rem;
+    font-size: 20px;
+  }
 </style>
